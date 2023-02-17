b3b02f7b8b9d8fd32e00f8be458ca7154fbdbca9
AngeloDiMortimer <angelodimortimer99@gmail.com>
2023-02-17 16:43:16 -0400

    Grid styling finished

--- src/menu.js
+++ src/menu.js
@@ -13,18 +13,28 @@ const createMenu = () => {
     
     const menu = document.createElement("div");
     menu.classList.add("menu");
-    
+    /* Introduce your menu here */
     menu.appendChild(addMenuItem(
        "Salsiccia",
        "Tomato sauce, Mozarella, Tomato, Sausage, Garlic, Basil" 
     ));
     
     menu.appendChild(addMenuItem(
-        "Salsiccia",
-        "Tomato sauce, Mozarella, Tomato, Sausage, Garlic, Basil" 
+        "Gamberi",
+        "Tomato sauce, Mozarella, Shrimps, Feta cheese, Olives, Basil" 
      ));
     
+     menu.appendChild(addMenuItem(
+        "Pepe",
+        "Tomato sauce, Mozarella, Chilli flakes, Olives, Basil" 
+     ));
     
+     menu.appendChild(addMenuItem(
+        "Pomodoro",
+        "Tomato sauce, Mozarella, Tomato, Onion, Feta cheese, Chilli" 
+     ));
+     
+
     return menu;
 }
 
@@ -35,16 +45,19 @@ const addMenuItem = (name, ingredients) => {
     const menuDiv = document.createElement("div");
     const foodName = document.createElement("h2");
     const foodDesc = document.createElement("p");
+    const imageWrapper= document.createElement("div");
     const foodImage = document.createElement("img");
 
     menuDiv.classList.add("menu-item");
+    imageWrapper.classList.add("img-wrapper");
 
     foodName.textContent = menuItem.name;
     foodDesc.textContent = menuItem.ingredients;
-    foodImage.src = 
+    foodImage.src = `img/pizzas/${menuItem.name.toLowerCase()}.jpg`
     foodImage.alt = `${menuItem.name}`;
 
-
+    imageWrapper.appendChild(foodImage);
+    menuDiv.appendChild(imageWrapper);
     menuDiv.appendChild(foodName);
     menuDiv.appendChild(foodDesc);
 
